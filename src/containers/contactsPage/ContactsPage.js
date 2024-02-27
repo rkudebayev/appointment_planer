04bfadda6a22abaaf449bbf98d62900f3575ede2
TourismTeacher <rasulito92@yandex.ru>
2024-02-27 16:17:12 +0600

    fixing form

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = ({contacts, addContact}) => {
+export const ContactsPage = ({contacts, addContacts}) => {
   /*
   Define state variables for 
   contact info and duplicate check
@@ -15,24 +15,14 @@ export const ContactsPage = ({contacts, addContact}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
     if (!duplicate) {
-      addContact(name, phone, email);
+      addContacts(name, phone, email);
       setName('');
       setEmail('')
       setPhone('');
     }
-
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
-  
   useEffect(() => {
     const nameIsDuplicate = () => {
       const found = contacts.find((contact) => contact.name === name);
@@ -52,8 +42,14 @@ export const ContactsPage = ({contacts, addContact}) => {
     <div>
       <section>
         { duplicate ? "Name already exists" : ""}
-        <ContactForm name={name} phone={phone} email={email} setName={setName} 
-        setPhone={setPhone} setEmail={setEmail} onSubmit={handleSubmit}/>
+        <ContactForm 
+        name={name} 
+        phone={phone} 
+        email={email} 
+        setName={setName} 
+        setPhone={setPhone} 
+        setEmail={setEmail} 
+        handleSubmit={handleSubmit}/>
       </section>
       <hr />
       <section>
